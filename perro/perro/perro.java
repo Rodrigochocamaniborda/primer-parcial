34baf65e925c676f44458297c12b47bb01c8f5a8
Rodrigochocamaniborda <81876505+Rodrigochocamaniborda@users.noreply.github.com>
2021-04-12 15:48:18 -0400

    Add files via upload

---
+++ perro/perro/perro.java
@@ -0,0 +1,37 @@
+
+
+public class perro
+{
+   String nombre;
+   String raza;
+   int edad;
+   String color;
+   public String setnombre(String nombre){
+    this.nombre= nombre;
+    return nombre;
+    }
+    public String getnombre(String nombre){
+    return nombre;
+    }
+   public String setraza(String raza){
+    this.raza= raza;
+    return raza;
+    }  
+   public String getraza(String raza){
+    return raza;
+    }
+    public int setedad(int edad ){
+    this.edad=edad;
+    return edad;
+    }
+    public int getedad(int edad){
+    return edad;
+    }
+    public String setcolor(String color){
+    this.color=color;
+    return color;
+    }
+    public String getcolor(String color){
+    return color;
+    }
+}
